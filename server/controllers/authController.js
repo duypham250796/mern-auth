3a17d261be84ac29b870d1cae97f1bf82a9cbd80
Duy Pham <duypham250796@gmail.com>
2024-12-29 18:09:56 +0700

    first commit

---
+++ server/controllers/authController.js
@@ -0,0 +1,29 @@
+import brcypt from "brcypt";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+export const register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.json({ success: false, message: `Missing Details` });
+    }
+
+    try {
+        const existingUser = await userModel.findOne({ email });
+
+        if (existingUser) {
+            return res.json({ success: false, message: `User already exists` });
+        }
+
+        const hashedPassword = await brcypt.hash(password, 10);
+
+        const user = new userModel({ name, email, password: hashedPassword });
+        await user.save();
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        res.cookie("token", token);
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+};
